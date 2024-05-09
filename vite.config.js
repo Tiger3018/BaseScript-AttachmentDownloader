114d63a2244a00bc9cf97c6a63320b4db96763b8
Zhou Bohan <git@tiger3018.top>
2024-05-09 19:03:45 +0800

    feat: add build ci.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
+    base: '/larkbase-attachment-downloader/',
     server: {
         host: '0.0.0.0',
     },
